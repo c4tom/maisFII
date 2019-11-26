ba876a95788e90a1b25edf18330487a43f640daf
Candido H Tominaga <candido1212@gmail.com>
2019-11-26 14:47:32 -0300

    Limpeza de codigo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MaisFII.Models;
 using MaisFII.Models.ViewModels;
 
 namespace MaisFII.Controllers
