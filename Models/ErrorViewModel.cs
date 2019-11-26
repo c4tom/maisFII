e7345677ba7800971e57ba3987c0c3ef2d125a76
Candido H Tominaga <candido1212@gmail.com>
2019-11-26 09:30:29 -0300

    Projeto criado - Dot Net - Core / MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MaisFII.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
